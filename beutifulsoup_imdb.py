8c90727ea5f4ead7bcbc8cf3bcfcf9dbd8e9e084
Berat Mert Kurun <83522079+BeratMert@users.noreply.github.com>
2021-05-03 23:01:24 +0300

    Add files via upload

---
+++ beutifulsoup_imdb.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.imdb.com/chart/top/"
+
+informations = open("movies.csv", "w")
+informations.write("movieNumber, movieName, date, rating\n")
+
+r = requests.get(url)
+soup = BeautifulSoup(r.content, "lxml")
+rows = soup.find_all("tr")
+
+print("*"*40)
+print("Top Rated Movies")
+print("*"*40)
+
+for row in rows:
+    titles = row.find_all("td", {"class":"titleColumn"})
+
+    for title in titles:
+        name = title.a.text
+        movieNumber = title.next.strip()
+        date = title.span.text
+        rating = row.strong.text
+        
+        print(f"{movieNumber}{name} {date}\nIMDB Rating; {rating}")
+        informations.write(movieNumber + "," + name + "," + date + "," + rating + "\n")
+
+    print("-"*50)
+
+informations.close()
